3865b49f79504312cb66ae5c1407ccc0ee865106
Yassine-Eljaouhary36 <yassinegogo36@gmail.com>
2023-08-21 02:47:18 +0100

    finish updates

---
+++ app/Jobs/OrderPaid.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class OrderPaid implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $order;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = $this->order->client;
+        try {
+            Mail::send('email.emailOrderInProcess', ['client' => $client,'order' => $this->order], function ($message) use ($client) {
+                $message->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+                $message->sender(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+                $message->to($client->email);
+                $message->replyTo(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+                $message->subject('Your Order Paid');
+                $message->priority(1);
+            });
+        } catch (\Exception $e) {
+            // if ($e->getMessage()) {
+                
+            // }
+        }
+    }
+}
